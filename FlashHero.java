a93888169e53cc6054752c0fa0f180af4b849970
ryzwar65 <riyanwarsetiadi0605@gmail.com>
2018-10-31 14:07:23 +0700

    Constructor

---
+++ FlashHero.java
@@ -0,0 +1,9 @@
+public class FlashHero extends Hero {
+	public FlashHero(){
+		super();
+		System.out.println("Iam The Flash");
+	}
+	public static void main(String[] args) {
+		FlashHero flash = new FlashHero();
+	}
+}
---
+++ Hero.java
@@ -0,0 +1,6 @@
+public class Hero {
+	public Hero(){
+		System.out.println("I Will Save You");
+		System.out.println("Your so Bad Zoom");
+	}
+}
